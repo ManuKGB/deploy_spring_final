058d2c83c4f4ea963898caaad41114dd0a6ac256
ManuKGB <kgb.corporation.2022@gmail.com>
2023-09-13 16:01:12 +0000

    new 13_09

--- src/test/java/kgbStage/api/ProjetStageApiApplicationTests.java
+++ src/test/java/kgbStage/api/ProjetStageApiApplicationTests.java
@@ -1,13 +1,21 @@
 package kgbStage.api;
 
+import kgbStage.api.services.EcoleServices;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class ProjetStageApiApplicationTests {
 
+	@Autowired
+	private EcoleServices yourService; // Remplacez YourService par la classe ou le composant que vous souhaitez tester
+
 	@Test
 	void contextLoads() {
+		// Vérifiez que le bean ou le composant est correctement autowired
+		assertThat(yourService).isNotNull();
 	}
-
 }
